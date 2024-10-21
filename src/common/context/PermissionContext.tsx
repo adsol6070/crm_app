486901a550f47489f8d2af3f8503874cada3ee8e
unknown <adsolforweb@gmail.com>
2024-10-21 12:20:04 +0530

    normal changes

--- src/common/context/PermissionContext.tsx
+++ src/common/context/PermissionContext.tsx
@@ -41,7 +41,6 @@ export const PermissionsProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
   const [permissions, setPermissions] = useState<Permissions>({});
-  console.log("Permissions of current role:", permissions);
   const { user } = useAuth();
   const isSuperAdmin = user?.role === "super_admin";
 
